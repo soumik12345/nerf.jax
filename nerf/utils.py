04ca8d387fca974b439acaecfc858cf00c7ac1ff
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2021-12-21 01:20:56 +0530

    added load data + training notebook

--- nerf/utils.py
+++ nerf/utils.py
@@ -1,7 +1,10 @@
 import os
+import wget
+import numpy as np
+from matplotlib import pyplot as plt
+
 import jax
 from jax.tools import colab_tpu
-from matplotlib import pyplot as plt
 
 
 def init_tpu():
@@ -17,3 +20,27 @@ def plot_results(images, titles, figure_size=(12, 12)):
         _ = plt.imshow(images[i])
         plt.axis("off")
     plt.show()
+
+
+def load_lego_data():
+    if not os.path.isfile("tiny_nerf_data.npz"):
+        wget.download(
+            "https://people.eecs.berkeley.edu/~bmild/nerf/tiny_nerf_data.npz"
+        )
+    data = np.load("tiny_nerf_data.npz")
+    images = data["images"]
+    poses = data["poses"]
+    focal = float(data["focal"])
+    _, image_height, image_width, _ = images.shape
+    test_image, test_pose = images[101], poses[101]
+    train_images = images[:100, ..., :3]
+    train_poses = poses[:100]
+    return (
+        train_images,
+        train_poses,
+        test_image,
+        test_pose,
+        focal,
+        image_height,
+        image_width,
+    )
